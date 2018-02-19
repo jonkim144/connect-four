3ee862ac576d06a66416a8d8198691ec4fae8644
Jonathan Kim <jon@than.kim>
2018-02-19 08:13:54 -0800

    Basic render test for board

---
+++ src/components/Board/Board.test.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Board from './Board';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Board />, div);
+});
